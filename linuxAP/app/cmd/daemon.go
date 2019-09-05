2f6ad109a1d21ce8e5a65b95bafb3317e5fbdf09
megaliao <micliao@tutanota.com>
2019-09-05 14:38:22 +0800

    add to repo

--- linuxAP/app/cmd/daemon.go
+++ linuxAP/app/cmd/daemon.go
@@ -23,13 +23,8 @@ import (
 // daemonCmd represents the daemon command
 var daemonCmd = &cobra.Command{
 	Use:   "daemon",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start as a daemon service",
+	Long: `start as a daemon service`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("daemon called")
 	},
