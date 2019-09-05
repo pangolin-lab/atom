781d3ccd19a5c21ed01ad7ae7897caba3d4f1382
megaliao <micliao@tutanota.com>
2019-09-05 11:18:37 +0800

    add to repo

--- linuxAP/test/testprivateip.go
+++ linuxAP/test/testprivateip.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
-	"github.com/kprc/nbsnetwork/tools/privateip"
+	"runtime"
+	"time"
 )
 
 func main()  {
-	fmt.Println(privateip.IsPrivateIPStr("192.168.1.1"))
-	fmt.Println(privateip.IsPrivateIPStr("172.16.1.23"))
-	fmt.Println(privateip.IsPrivateIPStr("172.168.11.1"))
-	fmt.Println(privateip.IsPrivateIPStr("230.12.1.1"))
-	fmt.Println(privateip.IsPrivateIPStr("100.84.1.2"))
+	//fmt.Println(privateip.IsPrivateIPStr("192.168.1.1"))
+	//fmt.Println(privateip.IsPrivateIPStr("172.16.1.23"))
+	//fmt.Println(privateip.IsPrivateIPStr("172.168.11.1"))
+	//fmt.Println(privateip.IsPrivateIPStr("230.12.1.1"))
+	//fmt.Println(privateip.IsPrivateIPStr("100.84.1.2"))
+
+
+
+	testfinalizer()
+
+
+}
+
+func testfinalizer()  {
+	fmt.Println("test 1")
+
+
+	defer func() {
+		fmt.Println("test defer")
+
+	}()
+
+	cnt:="test finalizer"
+
+	runtime.SetFinalizer(&cnt, func(s interface{}) {
+		fmt.Println(*(s.(*string)))
+	})
+
+	fmt.Println("before gc")
+	runtime.GC()
+	fmt.Println("end gc")
+
+
+
+	time.Sleep(time.Second*3)
 }
